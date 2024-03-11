9ffeb060c67a4874b7d22ed27c046fb0416dda7e
Tanish Dusane <91111180+DeathxHunter@users.noreply.github.com>
2024-03-11 23:55:06 +0530

    Encapsulation In Java Assignment Added Comments

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,11 +2,19 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
+        // Task 3: Creating an object of class RWOnly
         RWOnly obj = new RWOnly();
 
+        // Task 4: Trying to access the private member directly will result in a compile-time error
+        // System.out.println("Name: " + obj.name); // Error: name has private access in RWOnly
+
+        // Task 5: Using getter to access name (Initially, name is not set, so it will print null)
         System.out.println("Name: " + obj.getName());
 
+        // Task 6: Using setter to set name
         obj.setName("Tanish Dusane");
+
+        // Task 5: Using getter to access name after setting it
         System.out.println("Name: " + obj.getName());
     }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,13 +1,15 @@
 package com.driver;
 
 public class RWOnly {
-
+    // Task 2: Private member variable
     private String name;
 
+    // Task 5: Getter function
     public String getName() {
         return name;
     }
 
+    // Task 5: Setter function
     public void setName(String newName) {
         this.name = newName;
     }
