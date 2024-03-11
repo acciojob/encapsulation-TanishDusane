075ea1860a3ceb962b1773733c7f777b401b3a9e
Tanish Dusane <91111180+DeathxHunter@users.noreply.github.com>
2024-03-11 23:53:09 +0530

    Encapsulation In Java Assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+
+        System.out.println("Name: " + obj.getName());
+
+        obj.setName("Tanish Dusane");
+        System.out.println("Name: " + obj.getName());
+    }
 }
